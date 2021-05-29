21647abb9d612862c216e675365b296c5715a7de
haivt99 <52232584+haivt99@users.noreply.github.com>
2021-05-29 15:56:35 +0700

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,11 +6,14 @@ git commit -m "Up date a new file"
 git remote add origin https://github.com/haivt99/training_git
 git push -u origin master
 
-# Clone code về, sửa rồi push lên
+# Clone code về, edit rồi push lên
+git init
 git clone https://github.com/haivt99/training_git
-git checkout -b nameBranch
+cd training_git
+(edit file)
 git add nameFile
 git commit -m "Modify a file"
+git checkout -b nameBranch
 git push --set-upstream origin nameBranch
 
 # Lấy thay đổi từ trên server
